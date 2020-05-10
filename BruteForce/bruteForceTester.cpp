34f7b139a56395871587c50787a7993bf632eba4
shanewalsh1999 <shanewalsh1999@gmail.com>
2020-05-09 23:21:17 -0400

    Brute force tester now reads in instances from a file

--- BruteForce/bruteForceTester.cpp
+++ BruteForce/bruteForceTester.cpp
@@ -5,7 +5,9 @@
 
 using namespace std;
 
-#define numTestInstances 1
+// Work in progress. Still needs to be add function time limit and integration with algorithm.
+
+#define numTestInstances 2
 
 // Structure with data of BPP test instance
 struct instance {
@@ -15,12 +17,26 @@ struct instance {
     vector<int> items;
 };
 
+// printInstancesVector : vector of instances
+// Prints instances vector
+void printInstancesVector(vector<instance> instances) {
+    for (int i = 0; i < numTestInstances; i++) {
+        cout << "Instance name: " << instances[i].name << endl;
+        cout << "Capacity: " << instances[i].capacity << endl;
+        cout << "Items: <";
+            for(auto it = begin(instances[i].items); it != end(instances[i].items); ++it) {
+            std::cout << *it << " ";
+            }
+        cout << "> " << endl << endl;
+    }
+}
+
 int main() {
     // Initialize input stream
     ifstream inf;
 
     // Vector of 100 test instances
-    vector<instance> instances(100);
+    vector<instance> instances;
 
     // Open input file
     inf.open("testInstances.txt");
@@ -33,8 +49,46 @@ int main() {
     inf.exceptions(std::ifstream::failbit | std::ifstream::badbit);
 
     // Begin loop to read in 100 test instances
-    // for (int i = 0; i > numTestInstances; i++) {
+    for (int i = 0; i < numTestInstances; i++) {
+        instance dummy;
+        int weight, numOfOccurances;
+
+        // Read in name of instance
+        try { inf >> dummy.name; }
+        // Check for reading failure due to end of file
+        catch (std::ifstream::failure e) { cout << "ERROR: Formatting in instance" << i << "of input file\n"; } 
+        inf.clear();
+
+        // Read in number of unique weights of instance
+        try { inf >> dummy.uniqueWeights; }
+        catch (std::ifstream::failure e) { cout << "ERROR: Formatting in instance" << i << "of input file\n"; } 
+
+        // Read in capacity of instance
+        try { inf >> dummy.capacity; }
+        catch (std::ifstream::failure e) { cout << "ERROR: Formatting in instance" << i << "of input file\n"; } 
+
+        // Read in all weights of an instance
+        for (int j = 0; j < dummy.uniqueWeights; j++) {
+            // Read in weight
+            try { inf >> weight; }
+            catch (std::ifstream::failure e) { cout << "ERROR: Formatting in instance" << i << "of input file\n"; } 
+
+            // Read in number of occurances of weight
+            try { inf >> numOfOccurances; }
+            catch (std::ifstream::failure e) { cout << "ERROR: Formatting in instance" << i << "of input file\n"; } 
+
+            // Append appropriate occurances of weight to items vector
+            for (int k = 0; k < numOfOccurances; k++)
+                dummy.items.push_back(weight);
+        }
         
+        // Push dummy instance to vector of instances
+        instances.push_back(dummy);
+    }
+
+    // Print instances vector
+    printInstancesVector(instances);
+
     // Close input file
     inf.close();
 
